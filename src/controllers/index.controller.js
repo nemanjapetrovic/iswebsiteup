306d3e5868dee85eefa731902a92dbc3b6244da0
nemanjapetrovic <nempet@protonmail.com>
2019-11-15 00:01:23 +0100

    Removing comments and message response update

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -2,7 +2,7 @@ const child_process = require('child_process');
 
 const checkLib = require('./../sdk/lib/check.lib');
 
-const ok_codes = [200, 301, 302, 303, 304, 307, 400, 401, 403, 405];
+const okCodes = [200, 301, 302, 303, 304, 307, 400, 401, 403, 405];
 
 addCheck = async function (url, status) {
     checkLib.addCheck(url, status).catch(err => {
@@ -27,46 +27,18 @@ exports.checkUrl = async function (req, res) {
             responseStatus = response.substring(9, 12);
         }
 
-        const websiteUp = ok_codes.some(code => code == responseStatus) ? 1 : 0;
+        const websiteUp = okCodes.some(code => code == responseStatus) ? 1 : 0;
         addCheck(url, websiteUp);
 
-        res.status(200).send('ok');
-    } catch (err) {
-        addCheck(url, 0);
-        res.status(200).send('not ok');
-    }
-};
-
-/*
-    const url = req.query.url;
-    try {
-        const body = await got.head(url, {
-            timeout: 300,
-            retry: {
-                retries: 0
-            },
-            headers: {
-                'user-agent': 'IsWebsiteUp | check if website is down or up right now? (+https://iswebsiteup.com)'
-            },
-            followRedirect: false
+        res.status(200).json({
+            status: responseStatus,
+            message: 'Up'
         });
-
-        const websiteUp = ok_codes.some(code => code == body.statusCode) ? 1 : 0;
-        addCheck(url, websiteUp);
-
-        res.status(200).send('ok');
     } catch (err) {
         addCheck(url, 0);
-
-        if (err.code != 'ENOTFOUND') {
-            console.log(err);
-            res.sendStatus(500);
-            return false;
-        }
-
-        res.status(200).send('not ok');
+        res.status(200).json({
+            status: 'Unknown',
+            message: 'Down'
+        });
     }
 };
-//Help from: thanks!
-//https://github.com/sjparkinson/isitup.org/blob/master/src/functions.php
-*/
