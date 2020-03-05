7438194a2cc56e9c6c75e6901218d775c67fd254
nemanjapetrovic <nempet@protonmail.com>
2020-03-05 21:11:49 +0100

    Refactoring tests by defining new test suits

--- test/func.test.js
+++ test/func.test.js
@@ -6,290 +6,295 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('IsWebsiteUp tests', () => {
-
-  after(()=>{
+  after(() => {
     server.close();
   });
 
-  it('route /', (done) => {
-    chai.request(server)
-      .get('/')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        done();
-      });
-  });
-
-  it('400 bad req', (done) => {
-    chai.request(server)
-      .get('/someNotValidUrl')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(400);
-        done();
-      });
-  });
-
-  it('404 not found', (done) => {
-    chai.request(server)
-      .get('/notfound/notfound')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(404);
-        done();
-      });
-  });
-
-  it('[DOWN] [Timeout] gasas.com timeout', (done) => {
-    chai.request(server)
-      .get('/gasas.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 'Unknown',
-          message: 'Down'
+  describe('Basic routes tests', () => {
+    it('route /', (done) => {
+      chai.request(server)
+        .get('/')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          done();
         });
-        done();
-      });
+    });
+
+    it('400 bad req', (done) => {
+      chai.request(server)
+        .get('/someNotValidUrl')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(400);
+          done();
+        });
+    });
+
+    it('404 not found', (done) => {
+      chai.request(server)
+        .get('/notfound/notfound')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
   });
 
-  it('[DOWN] [Not Exist] aslkclakslkas.com check', (done) => {
-    chai.request(server)
-      .get('/aslkclakslkas.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 'Unknown',
-          message: 'Down'
+  describe('Test of websites which are DOWN', () => {
+    it('[Timeout] gasas.com timeout', (done) => {
+      chai.request(server)
+        .get('/gasas.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 'Unknown',
+            message: 'Down'
+          });
+          done();
         });
-        done();
-      });
+    });
+
+    it('[Not Exist] aslkclakslkas.com check', (done) => {
+      chai.request(server)
+        .get('/aslkclakslkas.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 'Unknown',
+            message: 'Down'
+          });
+          done();
+        });
+    });
+
+    it('[NO SSL] https://nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/https%3A%2F%2Fnikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 'Unknown',
+            message: 'Down'
+          });
+          done();
+        });
+    });
+
+    it('PORT check', (done) => {
+      chai.request(server)
+        .get('/nempet.me:8527')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 'Unknown',
+            message: 'Down'
+          });
+          done();
+        });
+    });
+
+    it('[NO SSL] https://www.nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/https%3A%2F%2Fwww.nikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 'Unknown',
+            message: 'Down'
+          });
+          done();
+        });
+    });
   });
 
-  it('[UP] hcp.switchplus.ch check', (done) => {
-    chai.request(server)
-      .get('/hcp.switchplus.ch')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 301,
-          message: 'Up'
+  describe('Test of websites which are UP', () => {
+    it('hcp.switchplus.ch check', (done) => {
+      chai.request(server)
+        .get('/hcp.switchplus.ch')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 301,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] google.com check', (done) => {
-    chai.request(server)
-      .get('/google.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 301,
-          message: 'Up'
+    it('google.com check', (done) => {
+      chai.request(server)
+        .get('/google.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 301,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] http://google.com check', (done) => {
-    chai.request(server)
-      .get('/http%3A%2F%2Fgoogle.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 301,
-          message: 'Up'
+    it('http://google.com check', (done) => {
+      chai.request(server)
+        .get('/http%3A%2F%2Fgoogle.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 301,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] https://google.com check', (done) => {
-    chai.request(server)
-      .get('/https%3A%2F%2Fgoogle.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 301,
-          message: 'Up'
+    it('https://google.com check', (done) => {
+      chai.request(server)
+        .get('/https%3A%2F%2Fgoogle.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 301,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] www.google.com check', (done) => {
-    chai.request(server)
-      .get('/www.google.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('www.google.com check', (done) => {
+      chai.request(server)
+        .get('/www.google.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] http://www.google.com check', (done) => {
-    chai.request(server)
-      .get('/http%3A%2F%2Fwww.google.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('http://www.google.com check', (done) => {
+      chai.request(server)
+        .get('/http%3A%2F%2Fwww.google.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] https://www.google.com check', (done) => {
-    chai.request(server)
-      .get('/https%3A%2F%2Fwww.google.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('https://www.google.com check', (done) => {
+      chai.request(server)
+        .get('/https%3A%2F%2Fwww.google.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/nikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/nikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] http://nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/http%3A%2F%2Fnikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('http://nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/http%3A%2F%2Fnikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[DOWN] [NO SSL] https://nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/https%3A%2F%2Fnikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 'Unknown',
-          message: 'Down'
+    it('www.nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/www.nikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] www.nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/www.nikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('http://www.nikos-hosting.com check', (done) => {
+      chai.request(server)
+        .get('/http%3A%2F%2Fwww.nikos-hosting.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[UP] http://www.nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/http%3A%2F%2Fwww.nikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
+    it('[Umlaut] städteflüge.com check', (done) => {
+      chai.request(server)
+        .get('/städteflüge.com')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 200,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
+    });
 
-  it('[DOWN] [NO SSL] https://www.nikos-hosting.com check', (done) => {
-    chai.request(server)
-      .get('/https%3A%2F%2Fwww.nikos-hosting.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 'Unknown',
-          message: 'Down'
+    it('LONG URL check', (done) => {
+      chai.request(server)
+        .get('/nempet.me%2Fmongoose-morgan%2Fmongoose-morgan.html')
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res.body).to.deep.equal({
+            status: 301,
+            message: 'Up'
+          });
+          done();
         });
-        done();
-      });
-  });
-
-  it('[UP] [Umlaut] städteflüge.com check', (done) => {
-    chai.request(server)
-      .get('/städteflüge.com')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 200,
-          message: 'Up'
-        });
-        done();
-      });
-  });
-
-  it('[UP] LONG URL check', (done) => {
-    chai.request(server)
-      .get('/nempet.me%2Fmongoose-morgan%2Fmongoose-morgan.html')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 301,
-          message: 'Up'
-        });
-        done();
-      });
-  });
-
-  it('[DOWN] PORT check', (done) => {
-    chai.request(server)
-      .get('/nempet.me:8527')
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.deep.equal({
-          status: 'Unknown',
-          message: 'Down'
-        });
-        done();
-      });
+    });
   });
 });
