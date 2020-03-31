5e873385a7fdaafe31ef04521f2ef863dc4ea2a3
nemanjapetrovic <nempet@protonmail.com>
2020-03-31 21:56:51 +0200

    Renaming controller file name.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const indexController = require('./../controllers/index.controller');
+const indexController = require('./../controllers/index');
 
 const validateUrl = require('./../middleware/validation/url.validate');
 
