859dd6834ee775632ccf4a229fdd524f42ec22c3
nemanjapetrovic <nempet@protonmail.com>
2023-01-20 00:17:53 +0100

    Added missing SEO part to update Sitemap.xml file during build process.

--- scripts/build.js
+++ scripts/build.js
@@ -26,6 +26,7 @@ ncp('./src', './www', function (err) {
   console.log('\u2705 Done coping of src folder!');
 
   // #region SEO optimization pages
+  var sitemapXmlData = [];
   console.log('Generating SEO files');
   const originalViewPath = path.join(__dirname, '..', 'src', 'views', 'index.ejs');
   for (var seoFile of seoFiles) {
@@ -54,10 +55,31 @@ ncp('./src', './www', function (err) {
     const contentRegex = /<p class="headline">.*?<\/p>/g;
     data = data.replace(contentRegex, `<p class="headline">${seoFile.content.pageText}</p>`);
 
+    // sitemap.xml array
+    sitemapXmlData.push(
+      `<url>
+      <loc>https://iswebsiteup.com/${seoFile.file.name.replace('.ejs', '')}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <priority>0.9</priority>
+      </url>`
+    );
+
     fs.writeFileSync(newViewPath, data, 'utf8');
 
     console.log(`\t\u2705 Done ${seoFile.file.name}`);
   }
+  // #endregion
+
+  // #region sitemap.xml
+
+  const sitemapPath = path.join(__dirname, '..', 'www', 'public', 'sitemap.xml');
+  const sitemapRegex = /<\/urlset>/g;
+  let sitemapData = fs.readFileSync(sitemapPath, 'utf8');
+  sitemapData = sitemapData.replace(sitemapRegex, `${sitemapXmlData.join('')}</urlset>`);
+  fs.writeFileSync(sitemapPath, sitemapData, 'utf8');
+
+  console.log('\u2705 Done updating sitemap.xml with seo files.');
+
   // #endregion
 
   // #region Versioning JS/CSS
